7870b1527ce906a7b26838de1b976813d23beb85
MaxChang3 <36927158+MaxChang3@users.noreply.github.com>
2023-03-10 19:24:56 +0000

    Deploying to gh-pages from @ MaxChang3/Bilibili-Trackpad-Scroll-Reverser@25e788619c9ba8eab658c1d89bb13ea6ed66b77a 🚀

--- bilibili-trackpad-scroll-reverser.user.js
+++ bilibili-trackpad-scroll-reverser.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name       哔哩哔哩反转触控板
-// @namespace  http://zhangmaimai.com/
-// @version    2.0.2
-// @author     MaxChang3
-// @license    MIT
-// @icon       https://www.bilibili.com/favicon.ico
-// @match      https://www.bilibili.com/bangumi/play/*
-// @match      https://www.bilibili.com/video/*
-// @run-at     document-start
+// @name         哔哩哔哩触控板滚动反转
+// @namespace    http://zhangmaimai.com/
+// @version      2.0.3
+// @author       MaxChang3
+// @description  优化 b 站视频音量调节在触控板上的体验。使用此脚本后，        在 b 站视频全屏界面中，使用触控板向下滚动将减少音量。（未安装时为增大）
+// @license      MIT
+// @icon         https://www.bilibili.com/favicon.ico
+// @match        https://www.bilibili.com/bangumi/play/*
+// @match        https://www.bilibili.com/video/*
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
