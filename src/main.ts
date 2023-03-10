b420318c8bbf18c0083046f8f87b85f2615e331c
MaxChang3 <maxcfoo@gmail.com>
2023-03-10 14:32:38 +0800

    refactor: refactor with proxy

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,25 @@ if (player === null) throw new Error('Can not detect player')
 
 const orgin = EventTarget.prototype.addEventListener
 
-EventTarget.prototype.addEventListener = function (...args: Parameters<typeof orgin>) {
+type AddEventListener = typeof orgin
+
+interface EventListener<T extends Event> {
+  (evt: T): void
+}
+
+const applyHandler = <T extends AddEventListener>(target: T, thisArg: EventTarget, args: Parameters<T>) => {
   const [type, evt, ...rest] = args
-  if (this instanceof HTMLElement || !(evt instanceof Function) || type !== "mousewheel") return Reflect.apply(orgin, this, args)
-  const evtWrapper: EventListener = (e) => {
-    const wheelEvent = e as WheelEvent
-    if (!isFullScreen() || !isTrackpad(wheelEvent)) return Reflect.apply(evt, this, [wheelEvent])
-    const proxy = new Proxy(wheelEvent, {
+  if (thisArg instanceof HTMLElement || !(evt instanceof Function) || type !== "mousewheel") return Reflect.apply(target, thisArg, args)
+  const evtWrapper: EventListener<WheelEvent> = (e) => {
+    if (!isFullScreen() || !isTrackpad(e)) return Reflect.apply(evt, thisArg, [e])
+    const proxy = new Proxy(e, {
       get: (obj, prop) => (typeof prop === "symbol" || prop !== "wheelDelta")
         ? Reflect.get(obj, prop)
         : obj['deltaY'] * 10 // Considering that `wheelDelta` is deprecated
     })
-    return Reflect.apply(evt, this, [proxy])
+    return Reflect.apply(evt, thisArg, [proxy])
   }
-  return Reflect.apply(orgin, this, [type, evtWrapper, ...rest])
+  return Reflect.apply(target, thisArg, [type, evtWrapper, ...rest])
 }
+
+EventTarget.prototype.addEventListener = new Proxy(orgin, { apply: applyHandler })
