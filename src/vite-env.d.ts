d23457f083905d14828cbf8a83571a8351680acb
MaxChang3 <maxcfoo@gmail.com>
2023-03-11 03:01:55 +0800

    fix: remove useless type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,3 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-monkey/client" />
 //// <reference types="vite-plugin-monkey/global" />
-/// <reference types="./types/bilivideo.ts" />
-/// <reference types="./types/typed.ts" />
