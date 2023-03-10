0f8b143d4e705c97c4b797d5e4075605f14d0393
MaxChang3 <maxcfoo@gmail.com>
2023-03-10 13:52:19 +0800

    refactor: use Reflect.apply

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { isFullScreen, isKeyIn, isTrackpad } from "./utils"
+import { isFullScreen, isTrackpad } from "./utils"
 
-const player = document.querySelector<HTMLElement>('#playerWrap') 
+const player = document.querySelector<HTMLElement>('#playerWrap')
 
 if (player === null) throw new Error('Can not detect player')
 
@@ -8,16 +8,16 @@ const orgin = EventTarget.prototype.addEventListener
 
 EventTarget.prototype.addEventListener = function (...args: Parameters<typeof orgin>) {
   const [type, evt, ...rest] = args
-  if (this instanceof HTMLElement || !(evt instanceof Function) || type !== "mousewheel") return orgin.apply(this, args)
+  if (this instanceof HTMLElement || !(evt instanceof Function) || type !== "mousewheel") return Reflect.apply(orgin, this, args)
   const evtWrapper: EventListener = (e) => {
     const wheelEvent = e as WheelEvent
-    if (!isFullScreen() || !isTrackpad(wheelEvent)) return evt.apply(this, [wheelEvent])
+    if (!isFullScreen() || !isTrackpad(wheelEvent)) return Reflect.apply(evt, this, [wheelEvent])
     const proxy = new Proxy(wheelEvent, {
       get: (obj, prop) => (typeof prop === "symbol" || prop !== "wheelDelta")
-        ? isKeyIn(prop, obj) ? obj[prop] : undefined
+        ? Reflect.get(obj, prop)
         : obj['deltaY'] * 10 // Considering that `wheelDelta` is deprecated
     })
-    return evt.apply(this, [proxy])
+    return Reflect.apply(evt, this, [proxy])
   }
-  return orgin.apply(this, [type, evtWrapper, ...rest])
+  return Reflect.apply(orgin, this, [type, evtWrapper, ...rest])
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-export const isKeyIn = <T extends object>(key: PropertyKey, obj: T): key is keyof T => key in obj
-
 export const isFullScreen = () => !!(document.fullscreenElement)
 
 export const isTrackpad = (wheelEvent: WheelEvent) => wheelEvent.deltaY && Math.abs(wheelEvent.deltaY) < 100
