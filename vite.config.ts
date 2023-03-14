0c80408bceb65dafae9226b2c9d4e6f2d17a6fa4
MaxChang3 <maxcfoo@gmail.com>
2023-03-15 00:44:46 +0800

    feat: support for media list

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
         description: '优化 b 站视频音量调节在触控板上的体验。使用此脚本后，在 b 站视频全屏界面中，使用触控板向下滚动将减少音量。（未安装时为增大）',
         match: [
           'https://www.bilibili.com/bangumi/play/*',
-          'https://www.bilibili.com/video/*'
+          'https://www.bilibili.com/video/*',
+          "https://www.bilibili.com/list/*"
         ],
         icon: 'https://www.bilibili.com/favicon.ico',
         "run-at": "document-start"
