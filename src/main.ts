b905424d8674c27bac815849b6edcb55998b1b5b
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2025-12-02 19:25:03 +0800

    fix: replace App instantiation with mount function

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { setupHook } from "@/hook"
 import { registerMenus } from "@/menu"
 import { getMouseMinDelta } from "@/utils/data"
 import App from '@/components/Init.svelte'
+import { mount } from "svelte"
 
 setupHook()
 registerMenus()
 if (getMouseMinDelta() === undefined) {
     window.onload = () => {
         document.body.style.overflow = 'hidden'
-        new App({
+        mount(App, {
             target: (() => {
                 const app = document.createElement('div')
                 document.body.append(app)
