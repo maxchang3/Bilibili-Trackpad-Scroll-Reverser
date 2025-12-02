eea7d29654fde04ebc4ae13cfc2770562ca917d4
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2025-12-03 01:00:26 +0800

    feat: allow dynamic updating of MOUSE_MIN value

--- src/utils/detect.ts
+++ src/utils/detect.ts
@@ -1,10 +1,15 @@
 import { getMouseMinDelta } from "@/utils/data"
 
-const MOUSE_MIN = getMouseMinDelta() || -1
+let MOUSE_MIN = getMouseMinDelta() || -1
 
 let container: HTMLDivElement
 
 console.log(`[BILIBILI-TRACKPAD-SCROLL-REVERSER] MOUSE_MIN: ${MOUSE_MIN}`)
+
+export const updateMouseMinDelta = (val: number | undefined) => {
+    MOUSE_MIN = val ?? -1
+    console.log(`[BILIBILI-TRACKPAD-SCROLL-REVERSER] Updated MOUSE_MIN: ${MOUSE_MIN}`)
+}
 
 export const isFullScreen = () => {
     if (!container) container = player.getElements().container
